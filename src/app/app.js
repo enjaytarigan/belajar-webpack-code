cc0b33f5209213ceb622efb472926aa0800a89ee
Enjay Tarigan <tariganagc@gmail.com>
2024-07-26 19:01:59 +0700

    implement common js module system

--- src/app/app.js
+++ src/app/app.js
@@ -1,30 +1,29 @@
-const alertService = new AlertService();
-const calculatorService = new CalculatorService();
-const jokesService = new JokesService();
+const cekInputValid = require("./utils/cekInputValid");
+const parseInput = require("./utils/parseInput");
 
 const run = (alertService, calculatorService, jokesService) => {
-  alertService.sembunyikanError();
-
-  calculatorService.onClick(() => {
     alertService.sembunyikanError();
-    const input = calculatorService.getInput();
-    const angka = parseInput(...input);
-    if (cekInputValid(...angka)) {
-      const [angka1, angka2] = angka;
-      calculatorService.setResult(angka1, angka2);
-    } else {
-      calculatorService.setResult('');
-      alertService.tampilkanErrorPenjumlahan(input, angka);
-    }
-  });
 
-  jokesService.onClick(() => {
-    fetch('https://candaan-api.vercel.app/api/text/random')
-      .then((response) => response.json())
-      .then((data) => {
-        jokesService.setModal(data.data);
-      });
-  });
+    calculatorService.onClick(() => {
+        alertService.sembunyikanError();
+        const input = calculatorService.getInput();
+        const angka = parseInput(...input);
+        if (cekInputValid(...angka)) {
+            const [angka1, angka2] = angka;
+            calculatorService.setResult(angka1, angka2);
+        } else {
+            calculatorService.setResult("");
+            alertService.tampilkanErrorPenjumlahan(input, angka);
+        }
+    });
+
+    jokesService.onClick(() => {
+        fetch("https://candaan-api.vercel.app/api/text/random")
+            .then((response) => response.json())
+            .then((data) => {
+                jokesService.setModal(data.data);
+            });
+    });
 };
 
-run(alertService, calculatorService, jokesService);
+module.exports = run;
